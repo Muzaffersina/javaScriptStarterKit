69442466f7b5955e6d3aabefe180f63e3920e322
Muzaffer Sina Açıkgöz <muzaffersina98@gmail.com>
2022-06-10 23:15:11 +0300

    mapFilterReduce

---
+++ src/mapFilterReduce.js
@@ -0,0 +1,38 @@
+// Single Page App (SPA)
+let cart = [
+    {id:1,productName: "Telefon",quantity:"3",unitPrice:4000},
+    {id:2,productName: "Bardaj",quantity:"2",unitPrice:30},
+    {id:3,productName: "Kalem",quantity:"1",unitPrice:20},
+    {id:4,productName: "Şarj Cihazı",quantity:"2",unitPrice:100},
+    {id:5,productName: "Kitap",quantity:"3",unitPrice:30},
+    {id:6,productName: "Pot",quantity:"5",unitPrice:150} 
+]
+
+
+cart.map(product =>{
+    console.log(product.productName +" : "+ product.unitPrice*product.quantity)
+})
+
+let total = cart.reduce((acc,product) => acc+product.unitPrice * product.quantity,0)
+console.log(total)
+
+let quantityOver2 = cart.filter(product=>product.quantity>2)
+console.log(quantityOver2)
+
+
+function addToCart(sepet) {
+    sepet.push({id:7,productName: "Süpürge",quantity:"7",unitPrice:3250})        
+}
+
+addToCart(cart)
+console.log(cart)
+
+// ********************************************************  Deger ve ref tip
+let sayi = 10 
+
+function sayiTopla(number) {
+    number += 1
+}
+
+sayiTopla(sayi)
+console.log(sayi)
