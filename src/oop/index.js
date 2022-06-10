30ee5f2730dbde85bbf34e48809148e46fb1101c
Muzaffer Sina Açıkgöz <muzaffersina98@gmail.com>
2022-06-10 17:30:40 +0300

    rest,spread,destructring,prototyping

---
+++ src/oop/index.js
@@ -0,0 +1,35 @@
+// Projenin başlama kısmı index.html de belirtilir.
+
+class Customer{
+    constructor(id,customerNumber){
+        this.id = id
+        this.customerNumber = customerNumber
+    }
+}
+
+let customer = new Customer(1,"12345")
+
+//inst prototyping
+customer.name = "Murat"
+console.log(customer.name)
+
+//class a prototyping
+Customer.bisey = "bisey"
+console.log(Customer.bisey)
+
+console.log(customer.customerNumber)
+
+class IndividualCustomer extends Customer{
+    constructor(firstName,id,customerNumber){
+        super(id,customerNumber)
+        this.firstName = firstName
+    }
+
+}
+
+class CorporateCustomer extends Customer{
+    constructor(companyName,id,customerNumber){
+        super(id,customerNumber)
+        this.companyName = companyName
+    }
+}
